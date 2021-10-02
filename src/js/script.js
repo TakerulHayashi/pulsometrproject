58835d496ecdd2b127a03fdad75420b27a9f37c4
TakerulHayashi <itstudentkz2000@gmail.com>
2021-10-02 12:04:59 +0600

    fifth commit

--- src/js/script.js
+++ src/js/script.js
@@ -20,5 +20,22 @@ $(document).ready(function(){
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+
+        // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+    
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
 
